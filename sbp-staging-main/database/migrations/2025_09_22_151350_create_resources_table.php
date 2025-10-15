d7c14c8feab901d62427897f9681ae220e59e6a4
heropaulexy <okeoluwaseun79@gmail.com>
2025-10-15 17:23:13 +0100

    new

--- sbp-staging-main/database/migrations/2025_09_22_151350_create_resources_table.php
+++ sbp-staging-main/database/migrations/2025_09_22_151350_create_resources_table.php
@@ -9,7 +9,7 @@ return new class extends Migration
     /**
      * Run the migrations.
      */
-    public function up(): void
+   public function up(): void
 {
     Schema::create('resources', function (Blueprint $table) {
         $table->uuid('id')->primary();
